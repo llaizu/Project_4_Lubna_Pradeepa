c77a1a2b15feed14070dc57e7e615368f7713e6b
pradeepa panneerselvam <pradeepaiit.95@gmail.com>
2021-08-02 16:40:07 +1000

    created tables and index

---
+++ SQL/create-seed-tables.sql
@@ -0,0 +1,9 @@
+INSERT INTO schedules(id, user_id, dayoftheweek, starttime, endtime)
+VALUES 
+('1', '1', '5', '2021-01-01 02:00:0', '2021-01-01 03:00:01'),
+('2', '2', '4', '2021-01-02 03:00:0', '2021-01-02 04:00:01');
+
+INSERT INTO users(user_id, surname, firstname, email_address, password)
+VALUES
+('1', 'Depp', 'Johnny', 'Johnny@gmail.com', 'hdckjhsdjkchj'),
+('2', 'Lyndon', 'James', 'Lyndon@gmail.com', 'gbdchjsbjchb');
