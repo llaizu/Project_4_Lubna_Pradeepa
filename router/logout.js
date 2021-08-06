c70de5ab69e999d0151720ce80661313e8014fa4
unknown <llaizu@yahoo.com.au>
2021-08-07 08:50:52 +1000

    Log out system installed and created links in headers

---
+++ router/logout.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+
+// Destroy the cookie and redirect to login page
+router.get("/", (req, res) => {
+	req.session.destroy(function (err) {
+		if (err) {
+			res.send(err.message)
+		} else {
+			res.clearCookie("coffee_sid")
+			res.redirect("/login")
+		}
+	})
+})
+
+module.exports = router
